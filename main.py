9d9405c8ccae46d182fe26319a40365d2a4c2d84
CodingDoongee <ohtaedong@kaist.ac.kr>
2020-08-27 23:26:11 +0900

    Progress

--- main.py
+++ main.py
@@ -1,105 +1,169 @@
-
 import numpy as np
-import matplotlib.pyplot as plt
 import h5py
-import scipy
-from PIL import Image
-from scipy import ndimage
-from lr_utils import load_dataset
+import matplotlib.pyplot as plt
+from testCases_v4a import *
+from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+np.random.seed(1)
 
 
-# Loading the data (cat/non-cat)
-train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+# GRADED FUNCTION: initialize_parameters
 
-# Example of a picture
-index = 22
-plt.imshow(train_set_x_orig[index])
-print ("y = " + str(train_set_y[:, index]) + ", it's a '"
-       + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
-plt.show()
-
-### START CODE HERE ### (≈ 3 lines of code)
-m_train = train_set_x_orig.shape[0]
-m_test = test_set_x_orig.shape[0]
-num_px = train_set_x_orig.shape[2]
-### END CODE HERE ###
-
-print ("Number of training examples: m_train = " + str(m_train))
-print ("Number of testing examples: m_test = " + str(m_test))
-print ("Height/Width of each image: num_px = " + str(num_px))
-print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-print ("train_set_x shape: " + str(train_set_x_orig.shape))
-print ("train_set_y shape: " + str(train_set_y.shape))
-print ("test_set_x shape: " + str(test_set_x_orig.shape))
-print ("test_set_y shape: " + str(test_set_y.shape))
-
-
-
-# Reshape the training and test examples
-
-### START CODE HERE ### (≈ 2 lines of code)
-train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -3).T
-test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -3).T
-### END CODE HERE ###
-
-print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
-print ("train_set_y shape: " + str(train_set_y.shape))
-print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
-print ("test_set_y shape: " + str(test_set_y.shape))
-print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
-
-train_set_x = train_set_x_flatten/255.
-test_set_x = test_set_x_flatten/255.
-
-
-# GRADED FUNCTION: sigmoid
-def sigmoid(z):
+def initialize_parameters(n_x, n_h, n_y):
     """
-    Compute the sigmoid of z
-
-    Arguments:
-    z -- A scalar or numpy array of any size.
-
-    Return:
-    s -- sigmoid(z)
-    """
-
-    ### START CODE HERE ### (≈ 1 line of code)
-    s = 1/(1+np.exp(-z))
-    ### END CODE HERE ###
-
-    return s
-
-
-print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
-
-
-# GRADED FUNCTION: initialize_with_zeros
-
-def initialize_with_zeros(dim):
-    """
-    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
-
     Argument:
-    dim -- size of the w vector we want (or number of parameters in this case)
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
 
     Returns:
-    w -- initialized vector of shape (dim, 1)
-    b -- initialized scalar (corresponds to the bias)
+    parameters -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """
+
+    np.random.seed(1)
+
+    ### START CODE HERE ### (≈ 4 lines of code)
+    W1 = np.random.randn(n_h, n_x)*0.01
+    b1 = np.zeros((n_h,1))
+    W2 = np.random.randn(n_y, n_h)*0.01
+    b2 = np.zeros((n_y,1))
+    ### END CODE HERE ###
+
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+
+    return parameters
+
+parameters = initialize_parameters(3,2,1)
+# print("W1 = " + str(parameters["W1"]))
+# print("b1 = " + str(parameters["b1"]))
+# print("W2 = " + str(parameters["W2"]))
+# print("b2 = " + str(parameters["b2"]))
+
+# GRADED FUNCTION: initialize_parameters_deep
+
+
+def initialize_parameters_deep(layer_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the dimensions of each layer in our network
+
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+                    bl -- bias vector of shape (layer_dims[l], 1)
+    """
+
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims)  # number of layers in the network
+
+    for l in range(1, L):
+        ### START CODE HERE ### (≈ 2 lines of code)
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l],layer_dims[l-1])*0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l],1))
+        ### END CODE HERE ###
+
+        assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+    return parameters
+
+
+parameters = initialize_parameters_deep([5,4,3])
+# print("W1 = " + str(parameters["W1"]))
+# print("b1 = " + str(parameters["b1"]))
+# print("W2 = " + str(parameters["W2"]))
+# print("b2 = " + str(parameters["b2"]))
+
+# GRADED FUNCTION: linear_forward
+
+
+def linear_forward(A, W, b):
+    """
+    Implement the linear part of a layer's forward propagation.
+
+    Arguments:
+    A -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+
+    Returns:
+    Z -- the input of the activation function, also called pre-activation parameter
+    cache -- a python tuple containing "A", "W" and "b" ; stored for computing the backward pass efficiently
     """
 
     ### START CODE HERE ### (≈ 1 line of code)
-    w = np.zeros((dim,1))
-    b = 0
+    Z = np.dot(W,A)+b
     ### END CODE HERE ###
 
-    assert (w.shape == (dim, 1))
-    assert (isinstance(b, float) or isinstance(b, int))
+    assert (Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
 
-    return w, b
+    return Z, cache
 
-dim = 2
-w, b = initialize_with_zeros(dim)
-print ("w = " + str(w))
-print ("b = " + str(b))
+A, W, b = linear_forward_test_case()
 
+Z, linear_cache = linear_forward(A, W, b)
+#print("Z = " + str(Z))
+
+
+# GRADED FUNCTION: linear_activation_forward
+
+def linear_activation_forward(A_prev, W, b, activation):
+    """
+    Implement the forward propagation for the LINEAR->ACTIVATION layer
+
+    Arguments:
+    A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+
+    Returns:
+    A -- the output of the activation function, also called the post-activation value
+    cache -- a python tuple containing "linear_cache" and "activation_cache";
+             stored for computing the backward pass efficiently
+    """
+
+    if activation == "sigmoid":
+        # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
+        ### START CODE HERE ### (≈ 2 lines of code)
+        Z, linear_cache = np.dot(W, A_prev)+b, A_prev
+        A, activation_cache = sigmoid(Z)
+        ### END CODE HERE ###
+
+    elif activation == "relu":
+        # Inputs: "A_prev, W, b". Outputs: "A, activation_cache".
+        ### START CODE HERE ### (≈ 2 lines of code)
+        Z, linear_cache = np.dot(W, A_prev)+b, A_prev
+        A, activation_cache = relu(Z)
+        ### END CODE HERE ###
+
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
+
+A_prev, W, b = linear_activation_forward_test_case()
+
+A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation = "sigmoid")
+print("With sigmoid: A = " + str(A))
+
+A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation = "relu")
+print("With ReLU: A = " + str(A))
